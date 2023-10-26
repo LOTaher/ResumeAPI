97d00346a3b4d74bf8d50f08ecdba29e80e9f1e3
Laith Taher <lotaher04@gmail.com>
2023-10-26 16:50:35 -0400

    Created more skills routes

--- handlers/skills_handler.go
+++ handlers/skills_handler.go
@@ -34,3 +34,87 @@ func GetSkills(db *sql.DB) func(c *fiber.Ctx) error {
 		return c.JSON(skills)
 	}
 }
+
+func GetSkillsByLanguages(db *sql.DB) func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		var skills models.Skills
+		query := "SELECT Languages FROM Skills LIMIT 1;"
+		var languages string
+
+		err := db.QueryRow(query).Scan(
+			&languages,
+		)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Could not get languages",
+			})
+		}
+
+		json.Unmarshal([]byte(languages), &skills.Languages)
+
+		return c.JSON(fiber.Map{"Languages": skills.Languages})
+	}
+}
+
+func GetSkillsByFrameworks(db *sql.DB) func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		var skills models.Skills
+		query := "SELECT Frameworks FROM Skills LIMIT 1;"
+		var frameworks string
+
+		err := db.QueryRow(query).Scan(
+			&frameworks,
+		)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Could not get frameworks",
+			})
+		}
+
+		json.Unmarshal([]byte(frameworks), &skills.Frameworks)
+
+		return c.JSON(fiber.Map{"Frameworks": skills.Frameworks})
+	}
+}
+
+func GetSkillsByTools(db *sql.DB) func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		var skills models.Skills
+		query := "SELECT DeveloperTools FROM Skills LIMIT 1;"
+		var developerTools string
+
+		err := db.QueryRow(query).Scan(
+			&developerTools,
+		)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Could not get developer tools",
+			})
+		}
+
+		json.Unmarshal([]byte(developerTools), &skills.DeveloperTools)
+
+		return c.JSON(fiber.Map{"Tools": skills.DeveloperTools})
+	}
+}
+
+func GetSkillsByLibraries(db *sql.DB) func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		var skills models.Skills
+		query := "SELECT Libraries FROM Skills LIMIT 1;"
+		var libraries string
+
+		err := db.QueryRow(query).Scan(
+			&libraries,
+		)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Could not get libraries",
+			})
+		}
+
+		json.Unmarshal([]byte(libraries), &skills.Libraries)
+
+		return c.JSON(fiber.Map{"Libraries": skills.Libraries})
+	}
+}
--- routes/routes.go
+++ routes/routes.go
@@ -15,8 +15,17 @@ func SetupRoutes(app *fiber.App, db *sql.DB) {
 
 	app.Get("/api/info", handlers.GetInfo(db))
 	app.Get("/api/education", handlers.GetEducation(db))
+
 	app.Get("/api/projects", handlers.GetProjects(db))
+    // app.Get("/api/projects/:id", handlers.GetProjectsById(db))
+
 	app.Get("/api/experience", handlers.GetExperience(db))
+    // app.Get("/api/experience/:id", handlers.GetExperienceById(db))
+
 	app.Get("/api/skills", handlers.GetSkills(db))
+    app.Get("/api/skills/languages", handlers.GetSkillsByLanguages(db))
+    app.Get("/api/skills/frameworks", handlers.GetSkillsByFrameworks(db))
+    app.Get("/api/skills/tools", handlers.GetSkillsByTools(db))
+    app.Get("/api/skills/libraries", handlers.GetSkillsByLibraries(db))
 
 }
